0f7d1756a6114e87d01a3dbde8b4b60b92837bab
Ayhan Ünal <unalayhan@yahoo.com>
2021-05-24 15:40:39 +0300

    app init

---
+++ PopupRememberChoice/PopupRememberChoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PopupRememberChoice
+//
+//  Created by Ayhan Ünal on 24.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
