d95dcfb0001ff39f6a82df9b8b0af1cd34e5d032
Ayhan Ünal <unalayhan@yahoo.com>
2021-05-24 15:53:26 +0300

    segue chck

--- PopupRememberChoice/PopupRememberChoice/ViewController.swift
+++ PopupRememberChoice/PopupRememberChoice/ViewController.swift
@@ -13,7 +13,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func secondPageClicked(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+        
+    }
+    
 
 }
 
