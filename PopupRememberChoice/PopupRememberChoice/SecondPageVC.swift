c89aba75188ff186592b046ea3f54b1c3b26af04
Ayhan Ünal <unalayhan@yahoo.com>
2021-05-24 15:46:37 +0300

    SecondPageVC added

---
+++ PopupRememberChoice/PopupRememberChoice/SecondPageVC.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPageVC.swift
+//  PopupRememberChoice
+//
+//  Created by Ayhan Ünal on 24.05.2021.
+//
+
+import UIKit
+
+class SecondPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
